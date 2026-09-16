8ada583467470ed2deb22058c4dfc951c158e62c
faza017 <faza.prasetyo10@gmail.com>
2026-09-16 19:15:33 +0300

    Firts commit

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-Nama  : Adhitya Warman <br/>
-NIM   : 124140007
+Nama  : M FAZA PRASETYO
+NIM   : 124140204
+PLATFORM : ANDROID
 
 Screnshoot Aplikasi :
-
- ![Image Alt](https://github.com/AW2606/MyFirstKMPApp/blob/ed4eb790c158442a62e59c4d34a7050c9dcb5ca1/Display.png)
--- shared/src/commonMain/kotlin/com/example/myfirstkmpapp/App.kt
+++ shared/src/commonMain/kotlin/com/example/myfirstkmpapp/App.kt
@@ -2,50 +2,40 @@ package com.example.myfirstkmpapp
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-@Preview
 fun App() {
     MaterialTheme {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(24.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+                .padding(horizontal = 20.dp, vertical = 32.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = "Halo, Adhitya Warman!",
-                fontSize = 28.sp
-            )
-
-            Spacer(
-                modifier = Modifier.height(12.dp)
+                " Nama : M Faza Prasetyo",
+                fontSize = 26.sp
             )
 
             Text(
-                text = "NIM: 124140007",
+                "NIM : 124140204",
+                modifier = Modifier.padding(top = 16.dp),
                 fontSize = 20.sp
             )
 
-            Spacer(
-                modifier = Modifier.height(12.dp)
-            )
-
             Text(
-                text = "Platform: Android",
+                "Platform : Android",
+                modifier = Modifier.padding(top = 10.dp),
                 fontSize = 18.sp
             )
         }
